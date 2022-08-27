bdef0ebd2b5f4a418913f0c965e98ee8bcbbc9c8
Jeremy Taylor <jdt@juxt.pro>
2022-08-27 23:50:03 +0100

    mariadb import instructions

--- mysql-sakila-db/mysql-sakila-schema.sql
+++ mysql-sakila-db/mysql-sakila-schema.sql
@@ -292,9 +292,10 @@ CREATE TABLE staff (
   last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY  (staff_id),
   KEY idx_fk_store_id (store_id),
-  KEY idx_fk_address_id (address_id),
-  CONSTRAINT fk_staff_store FOREIGN KEY (store_id) REFERENCES store (store_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_staff_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
+  KEY idx_fk_address_id (address_id)
+-- ,
+--  CONSTRAINT fk_staff_store FOREIGN KEY (store_id) REFERENCES store (store_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+--  CONSTRAINT fk_staff_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
